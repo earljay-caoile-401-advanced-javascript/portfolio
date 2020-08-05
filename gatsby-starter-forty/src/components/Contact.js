d5755a613715258a6eb38a334cafc5dcfe8cb801
Earl Jay Caoile <unfie555@gmail.com>
2020-08-05 12:20:52 -0700

    added some hardcoded content to page - figured out how to change/add routes

---
+++ gatsby-starter-forty/src/components/Contact.js
@@ -0,0 +1,63 @@
+import React from 'react'
+
+const Contact = props => (
+  <section id="contact">
+    <div className="inner">
+      <section>
+        <form method="post" action="#">
+          <div className="field half first">
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" id="name" />
+          </div>
+          <div className="field half">
+            <label htmlFor="email">Email</label>
+            <input type="text" name="email" id="email" />
+          </div>
+          <div className="field">
+            <label htmlFor="message">Message</label>
+            <textarea name="message" id="message" rows="6"></textarea>
+          </div>
+          <ul className="actions">
+            <li>
+              <input type="submit" value="Send Message" className="special" />
+            </li>
+            <li>
+              <input type="reset" value="Clear" />
+            </li>
+          </ul>
+        </form>
+      </section>
+      <section className="split">
+        <section>
+          <div className="contact-method">
+            <span className="icon alt fa-envelope"></span>
+            <h3>Email</h3>
+            <a href="mailto:earljay.caoile@gmail.com">
+              earljay.caoile@gmail.com
+            </a>
+          </div>
+        </section>
+        <section>
+          <div className="contact-method">
+            <span className="icon alt fa-phone"></span>
+            <h3>Phone</h3>
+            <span>(206) 513-4603</span>
+          </div>
+        </section>
+        <section>
+          <div className="contact-method">
+            <span className="icon alt fa-home"></span>
+            <h3>Location</h3>
+            <span>
+              Seattle, WA
+              <br />
+              United States of America
+            </span>
+          </div>
+        </section>
+      </section>
+    </div>
+  </section>
+)
+
+export default Contact
